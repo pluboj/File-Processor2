63f95bf1b257e0b2452e8884c30bba023d5d806f
pluboj <pluboj@yahoo.com>
2017-10-23 16:01:05 -0700

    Add text input elements

--- src/com/pl/Main.java
+++ src/com/pl/Main.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
@@ -36,6 +38,12 @@ public class Main extends Application {
         tocUrl.setToggleGroup(processGroup);
         tocUrl.setId("toc-url");
 
+        //URL input
+        TextField textUrl = new TextField();
+        textUrl.setPromptText("Paste Url");
+        textUrl.setPrefWidth(600);
+        textUrl.setEditable(false);
+
         //Grid Pane
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.TOP_LEFT);
@@ -43,12 +51,17 @@ public class Main extends Application {
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(25, 25, 25, 25));
 
+        //Button
+        Button openButton = new Button("Open Resource File");
+        openButton.setId("chooser-button");
+
         //Add elements to gridPane
         gridPane.add(title,0,0);
         gridPane.add(crFile,0,1);
         gridPane.add(tocFile,0,2);
         gridPane.add(tocUrl,0,3);
-
+        gridPane.add(textUrl,0,4);
+        gridPane.add(openButton, 0,6,2,1);
 
         //Scene
         Scene scene = new Scene(gridPane, 600, 275);
