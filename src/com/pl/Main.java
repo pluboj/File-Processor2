3101f121f6279a7e3344b1bef306819b153bdf35
pluboj <pluboj@yahoo.com>
2017-10-23 15:26:53 -0700

    Add controls

--- src/com/pl/Main.java
+++ src/com/pl/Main.java
@@ -1,31 +1,59 @@
 package com.pl;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("processor.fxml"));
         primaryStage.setTitle("File Processor v2.0");
-        primaryStage.setScene(new Scene(root, 600, 275));
+
+        //Title
+        Text title = new Text("File Processor");
+        title.setId("title");
 
         //Toggle group of radio buttons
         ToggleGroup processGroup = new ToggleGroup();
-        RadioButton tocFile = new RadioButton("Process ToC file");
+
+        RadioButton crFile = new RadioButton("Process CopyRecall File");
+        crFile.setToggleGroup(processGroup);
+        crFile.setId("cr-file");
+        crFile.setSelected(true);
+
+        RadioButton tocFile = new RadioButton("Process ToC File");
         tocFile.setToggleGroup(processGroup);
         tocFile.setId("toc-file");
 
+        RadioButton tocUrl = new RadioButton("Process ToC URL");
+        tocUrl.setToggleGroup(processGroup);
+        tocUrl.setId("toc-url");
+
+        //Grid Pane
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.TOP_LEFT);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+
+        //Add elements to gridPane
+        gridPane.add(title,0,0);
+        gridPane.add(crFile,0,1);
+        gridPane.add(tocFile,0,2);
+        gridPane.add(tocUrl,0,3);
 
 
-
-
+        //Scene
+        Scene scene = new Scene(gridPane, 600, 275);
+        scene.getStylesheets().add("com/pl/main.css");
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
