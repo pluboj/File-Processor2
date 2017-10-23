7edd61fc586a683f1ae556cf3aac23ab8e618cb3
pluboj <pluboj@yahoo.com>
2017-10-23 13:58:28 -0700

    Set basic layout

--- src/com/pl/Main.java
+++ src/com/pl/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("processor.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("File Processor v2.0");
+        primaryStage.setScene(new Scene(root, 600, 275));
+
+        //Toggle group of radio buttons
+        ToggleGroup processGroup = new ToggleGroup();
+        RadioButton tocFile = new RadioButton("Process ToC file");
+        tocFile.setToggleGroup(processGroup);
+        tocFile.setId("toc-file");
+
+
+
+
+
+
         primaryStage.show();
     }
 
